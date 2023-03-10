cd6bd639bdb8e11e8fd8d8d0e32b07cae348191d
Ravi Prakash <ravip140891@gmail.com>
2023-03-10 20:51:21 +0530

    Correction

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const errorHandler = require("./middelwares/errorMiddleware");
+import path from "path";
+import { fileURLToPath } from "url";
 
 //routes path
 const authRoutes = require("./routes/authRoutes");
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 //dotenv
 dotenv.config();
@@ -20,6 +26,7 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(errorHandler);
 
